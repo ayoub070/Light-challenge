4fffe8dc1b5373e2189b04973d749b6bf3706237
ayoub070 <93018211+ayoub070@users.noreply.github.com>
2023-09-15 15:59:57 +0200

    Code om 1 lampje rood te maken

--- Code_Light-challenge/LED/src/main.cpp
+++ Code_Light-challenge/LED/src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <FastLED.h>
+
+// How many leds in your strip?
+#define NUM_LEDS 1
+
+// For led chips like WS2812, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+#define DATA_PIN 3
+#define CLOCK_PIN 13
+
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+   // Uncomment/edit one of the following lines for your leds arrangement.
+    // ## Clockless types ##
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ // Turn the LED on, then pause
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(500);
+  // Now turn the LED off, then pause
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500);
 }
