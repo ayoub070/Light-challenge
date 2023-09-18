f2f16645d5977573527152405185803329bfe0cf
narwin98 <132271793+narwin98@users.noreply.github.com>
2023-09-18 11:19:56 +0200

    LEDs individueel aangestuurd oom stroom te meten

--- Code_Light-challenge/LED/src/main.cpp
+++ Code_Light-challenge/LED/src/main.cpp
@@ -3,7 +3,7 @@
 #include <FastLED.h>
 
 // How many leds in your strip?
-#define NUM_LEDS 1
+#define NUM_LEDS 16
 
 // For led chips like WS2812, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
@@ -21,16 +21,35 @@ void setup() {
    // Uncomment/edit one of the following lines for your leds arrangement.
     // ## Clockless types ##
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  
+    FastLED.setBrightness(100);  // setup the brightness of the leds
 }
 
 void loop() {
  // Turn the LED on, then pause
-  leds[0] = CRGB::Red;
+  leds[0] = CRGB::Blue;
+  leds[1] = CRGB::Blue;
+  leds[2] = CRGB::Blue;
+  leds[3] = CRGB::Red;
+  leds[4] = CRGB::DarkRed;
+  leds[5] = CRGB::OrangeRed;
+  leds[6] = CRGB::LightGoldenrodYellow;
+  leds[7] = CRGB::AntiqueWhite;
+  leds[8] = CRGB::Green;
+  leds[9] = CRGB::Yellow;
+  leds[10] = CRGB::Yellow;
+  leds[11] = CRGB::Yellow;
+  leds[12] = CRGB::MediumPurple;
+  leds[13] = CRGB::Purple;
+  leds[14] = CRGB::PowderBlue;
+  leds[15] = CRGB::HotPink;
   FastLED.show();
-  delay(500);
+  //delay(500);
+  
   // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
-  FastLED.show();
-  delay(500);
+ // leds[0] = CRGB::Black;
+  //FastLED.show();
+ // delay(500);
 }
+
+
+
