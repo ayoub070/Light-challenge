9bdc27ba0bbd049dba9e06cfc36425c23737acf6
narwin98 <132271793+narwin98@users.noreply.github.com>
2023-09-19 12:28:58 +0200

    Code coor PIR

--- Code_Light-challenge/LED/src/main.cpp
+++ Code_Light-challenge/LED/src/main.cpp
@@ -5,31 +5,45 @@
 // How many leds in your strip?
 #define NUM_LEDS 16
 
+
 // For led chips like WS2812, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
 // Clock pin only needed for SPI based chipsets when not using hardware SPI
 #define DATA_PIN 3
 #define CLOCK_PIN 13
+const byte PIR_sens = 4;
+const byte PIR_led  = 5;
+
+volatile byte state   = LOW;
+
+
 
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
+void pir_sens_L(); // declare the interrupt function
 
 void setup() {
+    
+    Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);   //initialize Safety light as output
+    attachInterrupt(digitalPinToInterrupt(PIR_sens),pir_sens_L,RISING);
    // Uncomment/edit one of the following lines for your leds arrangement.
     // ## Clockless types ##
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-    FastLED.setBrightness(100);  // setup the brightness of the leds
+    FastLED.setBrightness(50);  // setup the brightness of the leds
+
+   
 }
 
 void loop() {
  // Turn the LED on, then pause
-  leds[0] = CRGB::Blue;
-  leds[1] = CRGB::Blue;
-  leds[2] = CRGB::Blue;
-  leds[3] = CRGB::Red;
+  //leds[0] = CRGB::Blue;
+ // leds[1] = CRGB::Blue;
+ // leds[2] = CRGB::Blue;
+  /*leds[3] = CRGB::Red;
   leds[4] = CRGB::DarkRed;
   leds[5] = CRGB::OrangeRed;
   leds[6] = CRGB::LightGoldenrodYellow;
@@ -42,14 +56,33 @@ void loop() {
   leds[13] = CRGB::Purple;
   leds[14] = CRGB::PowderBlue;
   leds[15] = CRGB::HotPink;
-  FastLED.show();
+  FastLED.show();*/
   //delay(500);
+leds[0] = CRGB::Black;
+leds[1] = CRGB::Black;
+leds[2] = CRGB::Black;
+FastLED.show();
   
   // Now turn the LED off, then pause
  // leds[0] = CRGB::Black;
   //FastLED.show();
  // delay(500);
+//===============Code for the PIR sesnor================//
+
+
 }
 
+void pir_sens_L(){
+    //if (state==HIGH){
+        // leds[0] = CRGB::Blue;
+        // leds[1] = CRGB::Blue;
+       //  leds[2] = CRGB::Blue;
+       digitalWrite(LED_BUILTIN,HIGH);
+       //  FastLED.show();
+        delay(500);
+        Serial.println("aan");
+   // }
+    
+}
 
 
